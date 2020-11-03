5d76d778573496be46d0396538c17fc8a7f15224
obedsims <73711728+obedsims@users.noreply.github.com>
2020-11-03 22:11:34 +0000

    Update entsoe_client.py

--- entsoe_client.py
+++ entsoe_client.py
@@ -2,16 +2,17 @@ import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
 from entsoe import EntsoePandasClient
-from entsoe_token import token
+import os
 
 # Energy data is obtained from the ENTSOE API
 @st.cache
 def get_energy_data(country_code):
     load_dotenv()
-    client = EntsoePandasClient(api_key=token)
+    api_key = os.environ.get("token")
+    client = EntsoePandasClient(api_key=api_key)
     end = pd.Timestamp.now(tz='Europe/London')
     start = end - pd.DateOffset(months=1)
-    df = client.query_generation(country_code, start=start,end=end, psr_type=None)
-    #Ensuring the dataframe consists of an hourly frequency,
+    df = client.query_generation(country_code, start=start, end=end, psr_type=None)
+    #Ensuring the dataframe consists of an hourly frequency
     df = df.resample('H').mean()
     return df
