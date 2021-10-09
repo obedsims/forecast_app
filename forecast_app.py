8554ca832758cb3e65a1aac11a517525045a51fe
obedsims <73711728+obedsims@users.noreply.github.com>
2021-10-09 16:25:51 +0100

    Update forecast_app.py

--- forecast_app.py
+++ forecast_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from country_mappings import country_codes
+from country_mappings import COUNTRY_MAPPINGS
 from entsoe_client import get_energy_data
 from forecast import generate_forecast, calculate_smape
 
@@ -7,7 +8,7 @@ st.title("Forecasting Renewable Energy Generation in the UK")
 st.markdown("Chart One displays the total energy generation composition within the UK,\
              the other charts can be used as forecasts for solar and wind energy.")
 
-country = 'United Kingdom'
+country = list(COUNTRY_MAPPINGS.keys())
 
 regressor = st.sidebar.selectbox("Select a Regression Algorithm",
                                  ['Linear Regression', 'K-Nearest Neighbors',
